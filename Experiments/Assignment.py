4c4452bfc02bdd294a654be81b04f7bb78aa3b5d
bernalde <bernalde@cmu.edu>
2017-12-14 17:50:42 -0500

    Created assignment problem solving in Python

--- Experiments/Assignment.py
+++ Experiments/Assignment.py
@@ -10,117 +10,113 @@
 from gurobipy import *
 from numpy import *
 import os
-import glob
-
-
-# In[27]:
-
-
-directory = os.path.join("c:\\","path")
-
-for root,dirs,files in os.walk(directory):
-    for file in files:
-        if file.endswith(".csv"):
-            f=open(file, 'r')
-           #  perform calculation
-            f.close()
-
-
-# In[66]:
-
-
 import fnmatch
-import os
 
+
+# In[152]:
+
+
+cwd = os.getcwd()
 matches = []
 for root, dirnames, filenames in os.walk(cwd):
-    for filename in fnmatch.filter(filenames, 'Tij*.csv'):
+    for filename in fnmatch.filter(filenames, 'Pij_2.csv'):
         file_path = os.path.join(root, filename)
         P = loadtxt(open(root + '/' + filename, "rb"), delimiter=",")
         print root
+        print P
         numT = shape(P)[0]
-        numC = shape(P)[1]
+        try:
+            numC = shape(P)[1]
+        except:
+            numC = 0;    
 
+        print numC
         T=range(numT)
         C=range(numC)
-        
+
         # Initialize the model
         m=Model("Assignment")
-        
+
         # Define the binary variables Xs
         X = []
         for t in T:
             X.append([])
-            for c in C:
-                X[t].append(m.addVar(vtype=GRB.BINARY,name="X%d%d"% (t, c)))
+            if numC != 0:
+                for c in C:
+                    X[t].append(m.addVar(vtype=GRB.BINARY,name="X%d%d"% (t, c)))
+            else:
+                X[t].append(m.addVar(vtype=GRB.BINARY,name="X%d%d"% (t, 0)))
 
         # Set the optimization direction
         m.update()
         m.modelSense = GRB.MAXIMIZE
-        
-        
+
+
         # Define the assignment constraints
         constraintT = []
         constraintC = []
 
-        # One variable assigned per column
-        for t in range(numT):
-            constraintT.append(m.addConstr(quicksum(X[t][c] for c in range(numC))
-                                           <= 1 ,'constraintT%d' % t))
 
-        # One variable assigned per row
-        for c in range(numC):
-            constraintT.append(m.addConstr(quicksum(X[t][c] for t in range(numT))
-                                           <= 1 ,'constraintC%d' % t))
-        
-        
-        # Define objective
-        m.setObjective(quicksum(quicksum([X[t][c]*P[t][c] 
-                                for c in range(numC)]) 
-                                for t in range(numT)))
+        if numC != 0:
+            # One variable assigned per column
+            for t in range(numT):
+                constraintT.append(m.addConstr(quicksum(X[t][c] for c in range(numC))
+                                               <= 1 ,'constraintT%d' % t))
+            # One variable assigned per row
+            for c in range(numC):
+                constraintT.append(m.addConstr(quicksum(X[t][c] for t in range(numT))
+                                               <= 1 ,'constraintC%d' % t))
+
+            # Define objective
+            m.setObjective(quicksum(quicksum([X[t][c]*P[t][c] 
+                                    for c in range(numC)]) 
+                                    for t in range(numT)))
+        else:
+            # One variable assigned per column
+            for t in range(numT):
+                constraintT.append(m.addConstr(X[t][0]<= 1 ,'constraintT%d' % t))  
+
+            # One variable assigned per row
+            constraintT.append(m.addConstr(quicksum(X[t][0] for t in range(numT))
+                                               <= 1 ,'constraintC%d' % t))
+
+            # Define objective                 
+            m.setObjective(quicksum(X[t][0]*P[t] for t in range(numT)))
+
+
 
         m.update()
-        
+
         # Solve the optimization problem
         m.optimize()
-        
+
         # Print runtime and solution
 
         print 'runtime is',m.Runtime, 's'
+        print m
 
-        Xsol = zeros((numT,numC))
+
+        if numC != 0:
+            Xsol = zeros((numT,numC))
+        else:
+            Xsol = zeros(numT)
         # Print solution
         if m.status == GRB.Status.OPTIMAL:
             for t in range(numT):
-                for c in range(numC):
-                    if X[t][c].x > 0.5:
-                        print('%g -> %g' % (t, c))
-                        Xsol[t][c] = 1
+                if numC != 0:
+                    for c in range(numC):
+                        if X[t][c].x > 0.5:
+                            print('%g -> %g' % (t, c))
+                            Xsol[t][c] = 1
+                else:
+                    if X[t][0].x > 0.5:
+                        print('0 -> %g' % (t))
+                        Xsol[t] = 1
         print Xsol
-        name_sol = root + "/AssignmentT.csv"
-        savetxt(name_sol, Xsol, delimiter=",")
-
-
-# In[51]:
-
-
-
-
-
-# In[52]:
-
-
-
-
-
-# In[53]:
-
-
-
-
-
-# In[60]:
-
-
-
+        name_sol = root + "/Assignment.csv"
+        if numC != 0:
+            savetxt(name_sol, Xsol, delimiter=",")
+        else:
+            savetxt(name_sol, Xsol[None], delimiter=",")
+        
 
